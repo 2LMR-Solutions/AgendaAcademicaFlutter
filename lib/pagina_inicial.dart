3f04bc1ec23d19a90d0d83016d1c74144c652788
Matheus Benatti <matheus.cwb2@gmail.com>
2024-11-30 17:16:01 -0300

    fix: bloqueado o retorno para a pagina de login

--- lib/pagina_inicial.dart
+++ lib/pagina_inicial.dart
@@ -44,7 +44,7 @@ class _PaginaInicialState extends State<PaginaInicial> {
     final prefs = await SharedPreferences.getInstance();
     final atividadesJson = prefs.getStringList('atividades') ?? [];
     setState(() {
-      atividades = atividadesJson.map((json) => jsonDecode(json)).toList().cast<Map<String, dynamic>>();
+      atividades = atividadesJson.map((json) => jsonDecode(json)).toList().cast<Map<String, dynamic>>();;
     });
   }
 
@@ -92,7 +92,7 @@ class _PaginaInicialState extends State<PaginaInicial> {
                       subtarefa['marcada']
                           ? Icons.check_box
                           : Icons.check_box_outline_blank,
-                      color: Colors.blue,
+                      color: Color.fromRGBO(181, 33, 226, 1),
                     ),
                     title: Text(subtarefa['nome']),
                   );
@@ -113,179 +113,182 @@ class _PaginaInicialState extends State<PaginaInicial> {
     );
   }
 
+  // Função para bloquear o botão de voltar (pop)
+  Future<bool> _onWillPop() async {
+    // Retorna falso para bloquear a ação de voltar
+    return false;
+  }
 
   @override
   Widget build(BuildContext context) {
     ordenarAtividades(); // Ordena as atividades sempre que o widget for reconstruído
 
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color.fromRGBO(181, 33, 226, 1),
-        elevation: 10,
-        shadowColor: Colors.grey,
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(35),
-            bottomRight: Radius.circular(35),
+    return WillPopScope(
+      onWillPop: _onWillPop, // Bloqueia a navegação para a página anterior
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromRGBO(181, 33, 226, 1),
+          elevation: 10,
+          shadowColor: Colors.grey,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(35),
+              bottomRight: Radius.circular(35),
+            ),
+          ),
+          toolbarHeight: 170,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    'Olá,',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    nome,
+                    style: const TextStyle(
+                      fontSize: 25,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              IconButton(
+                icon: const Icon(
+                  Icons.account_circle,
+                  color: Colors.white,
+                  size: 70,
+                ),
+                onPressed: () {
+                  // Ação do botão (opcional)
+                },
+              ),
+            ],
+          ),
+          leading: Container(width: 50), // Mantém o espaço, mas remove a seta
+        ),
+        body: ConstrainedBox(
+          constraints: const BoxConstraints(maxHeight: 220, minHeight: 56.0),
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Atividades',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontFamily: 'Roboto',
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: atividades.isEmpty
+                    ? const Center(
+                  child: Text(
+                    'Nenhuma atividade cadastrada',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
+                  ),
+                )
+                    : ListView.builder(
+                  itemCount: atividades.length,
+                  itemBuilder: (context, index) {
+                    final atividade = atividades[index];
+                    return ListTile(
+                      leading: const Icon(Icons.add_task, color: Colors.green),
+                      title: Text(
+                        atividade['titulo'],
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      subtitle: Text(
+                        '${atividade['descricao']}\nData final: ${atividade['data']}',
+                      ),
+                      isThreeLine: true,
+                      trailing: const Icon(Icons.more_horiz),
+                      onTap: () {
+                        mostrarDetalhesAtividade(context, atividade);
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
           ),
         ),
-        toolbarHeight: 170,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Text(
-                  'Olá,',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                Text(
-                  nome,
-                  style: const TextStyle(
-                    fontSize: 25,
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-              ],
-            ),
-            IconButton(
-              icon: const Icon(
-                Icons.account_circle,
-                color: Colors.white,
-                size: 70,
-              ),
-              onPressed: () {
-                // Ação do botão (opcional)
-              },
-            ),
-          ],
-        ),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
+        floatingActionButton: FloatingActionButton(
           onPressed: () {
-            Navigator.pop(context);
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => TelaAtividade(
+                  adicionarAtividade: (atividade) {
+                    setState(() {
+                      atividades.add(atividade); // Adiciona a nova atividade à lista
+                      ordenarAtividades(); // Reordena após adicionar
+                      _salvarAtividades(); // Salva no SharedPreferences
+                    });
+                  },
+                ),
+              ),
+            );
           },
+          child: const Icon(Icons.add),
         ),
-      ),
-      body: ConstrainedBox(
-        constraints: const BoxConstraints(maxHeight: 220, minHeight: 56.0),
-        child: Column(
-          children: [
-            const Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'Atividades',
-                  style: TextStyle(
-                    fontSize: 15,
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w500,
-                    color: Colors.black,
-                  ),
-                ),
-              ),
+        bottomNavigationBar: BottomNavigationBar(
+          iconSize: 40,
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+          onTap: (index) {
+            switch (index) {
+              case 0:
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const PaginaInicial()),
+                );
+                break;
+              case 1:
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const AgendaPage()),
+                );
+                break;
+              case 2:
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const PerfilPage()),
+                );
+                break;
+            }
+          },
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: '',
             ),
-            Expanded(
-              child: atividades.isEmpty
-                  ? const Center(
-                child: Text(
-                  'Nenhuma atividade cadastrada',
-                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
-                ),
-              )
-                  : ListView.builder(
-                itemCount: atividades.length,
-                itemBuilder: (context, index) {
-                  final atividade = atividades[index];
-                  return ListTile(
-                    leading: const Icon(Icons.add_task, color: Colors.green),
-                    title: Text(
-                      atividade['titulo'],
-                      style: const TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text(
-                      '${atividade['descricao']}\nData final: ${atividade['data']}',
-                    ),
-                    isThreeLine: true,
-                    trailing: const Icon(Icons.more_horiz),
-                    onTap: () {
-                      mostrarDetalhesAtividade(context, atividade);
-                    },
-                  );
-                },
-              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_month),
+              label: '',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              label: '',
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => TelaAtividade(
-                adicionarAtividade: (atividade) {
-                  setState(() {
-                    atividades.add(atividade); // Adiciona a nova atividade à lista
-                    ordenarAtividades(); // Reordena após adicionar
-                    _salvarAtividades(); // Salva no SharedPreferences
-                  });
-                },
-              ),
-            ),
-          );
-        },
-        child: const Icon(Icons.add),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        iconSize: 40,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        onTap: (index) {
-          switch (index) {
-            case 0:
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const PaginaInicial()),
-              );
-              break;
-            case 1:
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const AgendaPage()),
-              );
-              break;
-            case 2:
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => const PerfilPage()),
-              );
-              break;
-          }
-        },
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_month),
-            label: '',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: '',
-          ),
-        ],
       ),
     );
   }
